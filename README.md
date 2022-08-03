0862419425e9219e3c653cba296aa96e0e62b868
Stillion <milosdograjic@yahoo.com>
2022-08-03 13:19:47 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# LoanCalculator
+ Entrance project for LeanPay
---
+++ src/main/java/com/leanpay/loancalculator/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.leanpay.loancalculator;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LoanCalculatorApplication.class);
+	}
+
+}
